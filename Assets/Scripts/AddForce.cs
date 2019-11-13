b1019b6846a5e9bf486fb25ae450e16841d7164f
Arun Kulshreshth <arunkul@louisiana.edu>
2019-11-13 01:11:26 -0600

    Initial Commit
    Unity Version 2019.2.1f1

---
+++ Assets/Scripts/AddForce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AddForce : MonoBehaviour {
+
+	Rigidbody rb;
+    public float force = 500.0f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Cursor.visible = true;
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	
+	void OnMouseDown()
+    {
+        rb.AddForce(transform.forward * force);
+        rb.useGravity = true;
+    }
+
+}
